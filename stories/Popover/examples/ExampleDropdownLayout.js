875ffb17de69eec2f02dd9a3a279065fb1b5c054
Yarden Sod-Moriah <yardnsm@gmail.com>
2018-12-03 14:25:58 +0200

    <Popove/> - fix smoke test; updated DropdownLayout example with custom styles

--- stories/Popover/examples/ExampleDropdownLayout.js
+++ stories/Popover/examples/ExampleDropdownLayout.js
@@ -4,6 +4,8 @@ import Popover from 'wix-style-react/Popover';
 import Button from 'wix-style-react/Button';
 import DropdownLayout from 'wix-style-react/DropdownLayout';
 
+import style from './ExampleDropdownLayout.st.css';
+
 class ExampleDropdownLayout extends React.Component {
   state = {
     selectedId: -1,
@@ -52,32 +54,33 @@ class ExampleDropdownLayout extends React.Component {
         <Popover
           dataHook="story-popover-dropdown-layout"
           shown={shown}
-          showArrow
           placement="bottom"
           appendTo="parent"
           onClickOutside={this.close}
           onKeyDown={this.onKeyDown}
+          {...style('root', {}, this.props)}
         >
           <Popover.Element>
             <Button onClick={this.toggle}>{selectedValue}</Button>
           </Popover.Element>
           <Popover.Content>
-            <div style={{ padding: '6px 0' }}>
-              <DropdownLayout
-                ref={r => (this.dropdownLayoutRef = r)}
-                minWidthPixels={200}
-                selectedId={selectedId}
-                onSelect={this.onSelect}
-                options={[
-                  { id: 0, value: 'Option 1' },
-                  { id: 1, value: 'Option 2' },
-                  { id: 2, value: 'Option 3' },
-                  { id: 3, value: 'Option 4' },
-                ]}
-                visible
-                inContainer
-              />
-            </div>
+            <DropdownLayout
+              ref={r => (this.dropdownLayoutRef = r)}
+              minWidthPixels={200}
+              selectedId={selectedId}
+              onSelect={this.onSelect}
+              options={[
+                { id: 0, value: 'Option 1' },
+                { id: 1, value: 'Option 2' },
+                { id: 2, value: 'Option 3' },
+                { id: 3, value: 'Option 4' },
+                { id: 4, value: 'Option 5' },
+                { id: 5, value: 'Option 6' },
+                { id: 6, value: 'Option 7' },
+              ]}
+              visible
+              inContainer
+            />
           </Popover.Content>
         </Popover>
       </div>
